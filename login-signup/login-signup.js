29d99297a570d227a5c4ff4a25f74bf318f5e16a
Josh-6 <Joshuabarpna@gmail.com>
2024-10-22 18:21:00 -0700

    Changed js so that both passwords are now toggles

--- login-signup/login-signup.js
+++ login-signup/login-signup.js
@@ -1,10 +1,13 @@
 const passwordInput = document.querySelector("#psw");
+const repeatPasswordInput = document.querySelector("#psw-repeat");
 const showPasswordCheckbox = document.querySelector("#showPassword");
 
 showPasswordCheckbox.addEventListener("change", function () {
     if (passwordInput.getAttribute("type") === "password") {
       passwordInput.setAttribute("type", "text");
+      repeatPasswordInput.setAttribute("type", "text");
     } else {
       passwordInput.setAttribute("type", "password");
+      repeatPasswordInput.setAttribute("type", "password");
     }
   });
