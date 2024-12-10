0418df281c650531a790cd741c462de6b1d053b0
Max <maxpdlt@outlook.com>
2024-12-10 15:04:31 -0800

    fixing issues

--- user-file/user-index.js
+++
@@ -1,4 +0,0 @@
-// Retrieve username from local storage
-const username = localStorage.getItem('username') || 'Guest'; // Assuming username is stored in local storage, defaults to Guest
-
-document.getElementById('username').textContent = username; // Update the username in the link
